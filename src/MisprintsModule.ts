c3ce1ad6d8c58b63aa09fd40b3dd2224514e8927
DorofeevMark <creekcreek760@gmail.com>
2019-02-27 21:02:07 +0300

    Changed indent size

--- src/MisprintsModule.ts
+++ src/MisprintsModule.ts
@@ -4,25 +4,25 @@
 import Service from './service';
 
 interface MisprintsConfig {
-    /** Number of picked reaction */
-    chatId: string;
+  /** Number of picked reaction */
+  chatId: string;
 }
 
 export default class Misprints {
-    public chatId: string;
+  public chatId: string;
 
-    constructor(config: MisprintsConfig) {
-        this.chatId = config.chatId;
-        window.addEventListener('keyup', this.notifyIfNeeded);
-    }
+  constructor (config: MisprintsConfig) {
+    this.chatId = config.chatId;
+    window.addEventListener('keyup', this.notifyIfNeeded);
+  }
 
-    private notifyIfNeeded(event) {
-        if (event.key === 'Enter') {
-            const selection = window.getSelection();
-            if (selection.toString().length) {
-                const fragmentPosition = selection.getRangeAt(0).getBoundingClientRect();
-                Service.notify(this.chatId, fragmentPosition.top, fragmentPosition.left)
-            }
-        }
+  private notifyIfNeeded (event) {
+    if (event.key === 'Enter') {
+      const selection = window.getSelection();
+      if (selection.toString().length) {
+        const fragmentPosition = selection.getRangeAt(0).getBoundingClientRect();
+        Service.notify(this.chatId, fragmentPosition.top, fragmentPosition.left);
+      }
     }
+  }
 }
